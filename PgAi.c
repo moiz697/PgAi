146d8d39378afc870aece95cde856a5887a3a03b
Moiz Ibrar <121784313+moiz697@users.noreply.github.com>
2023-10-30 21:31:33 +0500

    Update PgAi.c

--- PgAi.c
+++ PgAi.c
@@ -1,14 +1,48 @@
-#include <postgres.h>
-#include <fmgr.h>
-
+#include "postgres.h"
+/* OS Includes */
+/* PostgreSQL Includes */
 PG_MODULE_MAGIC;
+void _PG_init(void);
+void _PG_fini(void);
+Datum pg_all_queries(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(pg_all_queries);
+static void process_utility(PlannedStmt *pstmt, const char *queryString,ProcessUtilityContext context,ParamListInfo params,QueryEnvironment *queryEnv,DestReceiver *dest,       char *completionTag);
 
-// Define a function that your extension provides
-PG_FUNCTION_INFO_V1(my_extension_function);
-
-// Implementation of your extension function
-Datum my_extension_function(PG_FUNCTION_ARGS) {
-    // Your code here (replace this comment with your implementation)
-    PG_RETURN_NULL();
+void _PG_init(void)
+{
+    /* ... C code here at time of extension loading ... */
+    ProcessUtility_hook = process_utility;
 }
 
+Void _PG_fini(void)
+{
+    /* ... C code here at time of extension unloading ... */
+}
+static void process_utility(PlannedStmt *pstmt, 
+                           const char *queryString,
+                           ProcessUtilityContext context,
+                           ParamListInfo params,
+                           QueryEnvironment *queryEnv,DestReceiver *dest,
+                           char *completionTag)
+{
+    /* ... C code here ... */
+    standard_ProcessUtility(pstmt,  &nbsp;
+                            queryString,  &nbsp;
+                            context,  &nbsp;
+                            params,  &nbsp;
+                            queryEnv,  &nbsp;
+                            dest, 
+                            completionTag);
+    /* ... C code here ... */
+}
+Datum pg_all_queries(PG_FUNCTION_ARGS)
+{
+    /* ... C code here ... */
+    tupstore = tuplestore_begin_heap(true, false, work_mem);
+    /* ... C code here ... */     
+    values[0] = CStringGetTextDatum(query);
+    values[1] = CStringGetTextDatum(pid);
+    /* ... C code here ... */
+    tuplestore_donestoring(tupstore);
+    return (Datum) 0;
+}
