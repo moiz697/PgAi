d283b6e88d78a90bb0c3195266bafe4dc1509d35
Moiz Ibrar <121784313+moiz697@users.noreply.github.com>
2023-11-28 20:06:19 +0500

    Update pgai.py

--- pgai.py
+++ pgai.py
@@ -1,196 +1,245 @@
+import psycopg2
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
+from sklearn.model_selection import KFold
 from tensorflow.keras.layers import Dense, LSTM
-import tensorflow as tf
-import math
 from sklearn.metrics import mean_squared_error
-from numpy import array
-# Replace 'your_file.csv' with the actual file path
-df = pd.read_csv('/Users/moizibrar/Downloads/AAPL.csv')
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+import math
 
-# Display the last few rows of the DataFrame to understand the structure
-print(df.tail())
 
-# Reset the index and extract the 'close' column
-df1 = df.reset_index()['close']
 
-# Plot the 'close' column
-plt.plot(df1)
 
-# Display the values of the 'close' column
-print(df1)
+import os
+import psycopg2
 
-# Normalize the 'close' values using Min-Max scaling
-scaler = MinMaxScaler(feature_range=(0, 1))
-df1 = scaler.fit_transform(np.array(df1).reshape(-1, 1))
+def connect_to_db():
+    # Load environment variables from .env file
+    load_dotenv()
 
-# Display the shape of the normalized data
-print(df1.shape)
+    # Get database connection details from environment variables
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
 
-# Define the size of the training set
-train_size = int(len(df1) * 0.65)
-test_size = len(df1) - train_size
+    # Create a PostgreSQL connection
+    connection = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        database=db_name,
+        user=db_user,
+        password=db_password
+    )
 
-# Create training and testing datasets
-train_data = df1[:train_size, :]
-test_data = df1[train_size:, :1]
+    return connection
 
-# Display the sizes of the training and testing datasets
-print(train_size)
-print(test_size)
-print(test_data)
+# Call the function to connect to the database
+connection = connect_to_db()
 
-def create_dataset(dataset, time_step=1):
-    """
-    Create sequences of input features (dataX) and corresponding target values (dataY)
-    from a given time series dataset.
+def load_and_prepare_data(connection):
+    # Get database connection details from the connection object
+    db_user = connection.info.user
+    db_password = connection.info.password
+    db_host = connection.info.host
+    db_port = connection.info.port
+    db_name = connection.info.dbname
 
-    Parameters:
-    - dataset: 1D numpy array representing the time series data.
-    - time_step: Number of time steps to consider for each sequence (default is 1).
+    # Create a new connection using SQLAlchemy
+    engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-    Returns:
-    - dataX: 2D numpy array representing input sequences.
-    - dataY: 1D numpy array representing target values.
-    """
+    # Rest of the function remains unchanged
+    sql_query = "SELECT * FROM stock_data"
+    df = pd.read_sql_query(sql_query, engine)
+    df1 = df.reset_index()['close']
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    df1 = scaler.fit_transform(np.array(df1).reshape(-1, 1))
+    train_size = int(len(df1) * 0.70)
+    test_size = len(df1) - train_size
+    train_data = df1[:train_size, :]
+    test_data = df1[train_size:, :1]
+    engine.dispose()
+    return df1, train_data, test_data, scaler
+
+def create_dataset(dataset, time_step=100):
     dataX, dataY = [], []
-
-    # Iterate over the dataset to create sequences
     for i in range(len(dataset) - time_step - 1):
-        # Extract a sequence of 'time_step' elements from the dataset
         a = dataset[i:(i + time_step), 0]
         dataX.append(a)
-
-        # The target value is the next element after the sequence
         dataY.append(dataset[i + time_step, 0])
-
-    # Convert the lists to numpy arrays
     return np.array(dataX), np.array(dataY)
 
-# Define the number of time steps for each sequence
-time_step = 100
+def create_and_train_model(x_train, y_train, x_test, y_test):
+    model = Sequential([
+        LSTM(50, return_sequences=True, input_shape=(100, 1)),
+        LSTM(50, return_sequences=True),
+        LSTM(50),
+        Dense(1)
+    ])
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=100, batch_size=64, verbose=1)
+    return model
 
-# Create training datasets using the create_dataset function
-x_train, y_train = create_dataset(train_data, time_step)
+def predict_and_evaluate(model, x_train, y_train, x_test, y_test, scaler):
+    train_predict = model.predict(x_train)
+    test_predict = model.predict(x_test)
+    train_predict = scaler.inverse_transform(train_predict)
+    test_predict = scaler.inverse_transform(test_predict)
+    rmse_train = math.sqrt(mean_squared_error(y_train, train_predict))
+    return train_predict, test_predict, rmse_train
 
-# Create testing datasets using the create_dataset function
-x_test, y_test = create_dataset(test_data, time_step)
+def plot_original_vs_predicted(df1, train_predict, test_predict, scaler):
+    # Inverse transform the original normalized data
+    original_data = scaler.inverse_transform(df1)
 
-# Print the shapes of the training and testing datasets
-print("Training data shapes:")
-print("Input sequences (x_train):", x_train.shape)
-print("Target values (y_train):", y_train.shape)
+    # Prepare the plot for the training predictions
+    train_predict_plot = np.empty_like(df1)
+    train_predict_plot[:, :] = np.nan
+    train_predict_plot[100:len(train_predict) + 100, :] = train_predict
 
-print("\nTesting data shapes:")
-print("Input sequences (x_test):", x_test.shape)
-print("Target values (y_test):", y_test.shape)
+    # Prepare the plot for the testing predictions
+    test_predict_plot = np.empty_like(df1)
+    test_predict_plot[:, :] = np.nan
+    test_predict_plot[len(train_predict) + (100 * 2) + 1:len(df1) - 1, :] = test_predict
 
-# Reshape x_train to be compatible with the input shape of a neural network (3D array)
-# The new shape is (number of samples, number of time steps, number of features per time step)
-x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.title('Original vs Predicted Stock Prices')
+    plt.xlabel('Time')
+    plt.ylabel('Stock Price')
+    plt.plot(original_data, label='Original Data')
+    plt.plot(train_predict_plot, label='Training Predictions')
+    plt.plot(test_predict_plot, label='Testing Predictions')
+    plt.legend()
+    plt.show()
 
-# Reshape x_test similarly to match the input shape of the neural network
-x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
+    print("Plotting Original vs Predicted Stock Prices")
+    print("Blue line represents the original stock prices.")
+    print("Orange line represents the training data predictions.")
+    print("Green line represents the testing data predictions.")
 
-# Create a sequential model
-model = Sequential()
+def plot_future_predictions(future_predictions):
+    if len(future_predictions) == 0:
+        print("No future predictions generated.")
+        return
 
-# Add the first LSTM layer with 50 units, return full sequences, and specify input shape
-model.add(LSTM(50, return_sequences=True, input_shape=(100, 1)))
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.title('Future Stock Price Predictions')
+    plt.xlabel('Time')
+    plt.ylabel('Stock Price')
+    plt.plot(future_predictions, label='Future Predictions', color='red')
+    plt.legend()
+    plt.show()
 
-# Add the second LSTM layer with 50 units and return full sequences
-model.add(LSTM(50, return_sequences=True))
+    print("Plotting Future Stock Price Predictions")
+    print("Red line represents the future predictions for the stock prices.")
 
-# Add the third LSTM layer with 50 units (final layer in the sequence)
-model.add(LSTM(50))
 
-# Add a dense output layer with 1 unit for regression task
-model.add(Dense(1))
+def plot_original_vs_predicted(df1, train_predict, test_predict, scaler):
+    # Inverse transform the original normalized data
+    original_data = scaler.inverse_transform(df1)
 
-# Compile the model, specifying mean squared error loss and Adam optimizer
-model.compile(loss='mean_squared_error', optimizer='adam')
+    # Prepare the plot for the training predictions
+    train_predict_plot = np.empty_like(df1)
+    train_predict_plot[:, :] = np.nan
+    train_predict_plot[100:len(train_predict) + 100, :] = train_predict
 
-# Display the model summary
-model.summary()
+    # Prepare the plot for the testing predictions
+    test_predict_plot = np.empty_like(df1)
+    test_predict_plot[:, :] = np.nan
+    test_predict_plot[len(train_predict) + (100 * 2) + 1:len(df1) - 1, :] = test_predict
 
-# Train the model on the training data with validation on the testing data
-model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=100, batch_size=64, verbose=1)
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.title('Original vs Predicted Stock Prices')
+    plt.xlabel('Time')
+    plt.ylabel('Stock Price')
+    plt.plot(original_data, label='Original Data')
+    plt.plot(train_predict_plot, label='Training Predictions')
+    plt.plot(test_predict_plot, label='Testing Predictions')
+    plt.legend()
+    plt.show()
 
-# Check the TensorFlow version
-print(tf.__version__)
+    print("Plotting Original vs Predicted Stock Prices")
+    print("Blue line represents the original stock prices.")
+    print("Orange line represents the training data predictions.")
+    print("Green line represents the testing data predictions.")
 
-# Predict on the training and testing datasets
-train_predict = model.predict(x_train)
-test_predict = model.predict(x_test)
+def plot_future_prediction(df1, future_predictions, scaler):
+    # Inverse transform the future predictions to the original scale
+    future_predictions_transformed = scaler.inverse_transform(future_predictions.reshape(-1, 1))
 
-# Inverse transform the predictions to the original scale
-train_predict = scaler.inverse_transform(train_predict)
-test_predict = scaler.inverse_transform(test_predict)
+    # Define the range for the future predictions
+    last_data_point = len(df1)
+    future_range = np.arange(last_data_point, last_data_point + len(future_predictions))
 
-# Calculate and display the Root Mean Squared Error (RMSE) for the training data
-rmse_train = math.sqrt(mean_squared_error(y_train, train_predict))
-print("Root Mean Squared Error (RMSE) for Training Data:", rmse_train)
+    # Plotting
+    plt.figure(figsize=(12, 6))
+    plt.title('Future Stock Price Predictions')
+    plt.xlabel('Time')
+    plt.ylabel('Stock Price')
+    plt.plot(future_range, future_predictions_transformed, label='Future Predictions', color='red')
+    plt.legend()
+    plt.show()
 
-look_back=100
-trainPredictPlot=np.empty_like(df1)
-trainPredictPlot[:,:]=np.nan
-trainPredictPlot[look_back:len(train_predict)+look_back,:]=train_predict
-testPredictPlot=np.empty_like(df1)
-testPredictPlot[len(train_predict)+(look_back*2)+1:len(df1)-1,:]=test_predict
-plt.plot(scaler.inverse_transform(df1))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
-plt.show()
-x_input=test_data[341:].reshape(1,-1)
-x_input.shape
-len(test_data)
-x_input=test_data[341:].reshape(1,-1)
-x_input.shape
-temp_input = list(x_test)
-temp_input = temp_input[0].tolist()
-lst_output=[]
-n_steps=100
-i=0
-temp_input = list(x_test)
-temp_input = temp_input[0].tolist()
-lst_output = []
-n_steps = 100
-i = 0
-while i < 30:
-    if len(temp_input) > n_steps:
-        x_input = np.array(temp_input[-n_steps:])
-        print("{} day input {}".format(i, x_input))
-        x_input = x_input.reshape((1, n_steps, 1))
-        yhat = model.predict(x_input, verbose=0)
-        print("{} day output {}".format(i, yhat))
-        temp_input = np.append(temp_input, yhat[0, 0])
-        lst_output = np.append(lst_output, yhat[0, 0])
-        i = i + 1
-    else:
-        x_input = np.array(temp_input[-n_steps:])
-        x_input = x_input.reshape((1, n_steps, 1))
-        yhat = model.predict(x_input, verbose=0)
-        print("{} day output {}".format(i, yhat[0]))
-        temp_input = np.append(temp_input, yhat[0, 0])
-        lst_output = np.append(lst_output, yhat[0, 0])
-        i = i + 1
-temp_input = np.array(temp_input)
-lst_output = np.array(lst_output)   
+    print("Plotting Future Stock Price Predictions")
+    print("Red line represents the future predictions for the stock prices.")
 
-print(lst_output)
+    return future_predictions_transformed  # Add this line to return the transformed predictions
 
-day_new = np.arange(1, 101)
-day_pred = np.arange(101, 131)
-len(df1)
-plt.plot(day_new,scaler.inverse_transform(df1[1158:]))
-plt.plot(day_pred,scaler.inverse_transform(lst_output))
 
-df3 = df1.tolist()
-df3.extend(lst_output)
-plt.plot(df3[1200:])
-df3 = scaler.inverse_transform(df3).tolist()
-plt.plot(df3)
-plt.show()
+def calculate_rmse(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    return rmse
+
+def apply_kfold_cross_validation(model, X, y, n_splits=5):
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+    rmse_values = []
+
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+
+        rmse = calculate_rmse(y_test, y_pred)
+        rmse_values.append(rmse)
+
+    return rmse_values
+
+connection = connect_to_db()
+if connection:
+    df1, train_data, test_data, scaler = load_and_prepare_data(connection)
+    x_train, y_train = create_dataset(train_data)
+    x_test, y_test = create_dataset(test_data)
+    model = create_and_train_model(x_train, y_train, x_test, y_test)
+    train_predict, test_predict, rmse_train = predict_and_evaluate(model, x_train, y_train, x_test, y_test, scaler)
+    lst_output = plot_future_prediction(df1, test_predict, scaler)
+    plot_future_prediction(df1, lst_output, scaler)
+    rmse_train = calculate_rmse(y_train, train_predict)
+    rmse_test = calculate_rmse(y_test, test_predict)
+
+    # Print RMSE values
+    print("RMSE for Training Data:", rmse_train)
+    print("RMSE for Testing Data:", rmse_test)
+    # Apply K-fold cross-validation on the training data
+    rmse_values = apply_kfold_cross_validation(model, x_train, y_train)
+
+    # Print average RMSE over all folds
+    print("Average RMSE over K folds:", np.mean(rmse_values))
+
+
+    train_predict, test_predict = predict_and_evaluate(model, x_train, y_train, x_test, y_test, scaler)[:2]
+    
+    plot_original_vs_predicted(df1, train_predict, test_predict, scaler)
+    plot_future_predictions(lst_output)
+    connection.close()
