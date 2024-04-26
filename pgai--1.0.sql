ecfcfbdbfff4d68f13d17198352f6b7032850946
Moiz Ibrar <121784313+moiz697@users.noreply.github.com>
2024-04-27 01:38:36 +0500

    Update pgai--1.0.sql

--- pgai--1.0.sql
+++ pgai--1.0.sql
@@ -3,17 +3,44 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgai" to load this file. \quit
 
-CREATE FUNCTION pg_test (
+CREATE FUNCTION apple_stock (
 out date    date,
 out open    numeric,
 out high    numeric,
 out low     numeric,
 out close   numeric,
 out volume  bigint,
-out name    text,
+
 out close_pred    int
 )
 RETURNS SETOF record
-AS 'MODULE_PATHNAME', 'pg_test'
+AS 'MODULE_PATHNAME', 'apple_stock'
 LANGUAGE C STRICT VOLATILE PARALLEL SAFE;
 
+CREATE FUNCTION tesla_stock (
+out date    date,
+out open    numeric,
+out high    numeric,
+out low     numeric,
+out close   numeric,
+out volume  bigint,
+
+out close_pred    int
+)
+RETURNS SETOF record
+AS 'MODULE_PATHNAME', 'tesla_stock'
+LANGUAGE C STRICT VOLATILE PARALLEL SAFE;
+
+CREATE FUNCTION msci_pak_global_stock (
+out date    date,
+out open    numeric,
+out high    numeric,
+out low     numeric,
+out close   numeric,
+out volume  bigint,
+
+out close_pred    int
+)
+RETURNS SETOF record
+AS 'MODULE_PATHNAME', 'msci_pak_global_stock'
+LANGUAGE C STRICT VOLATILE PARALLEL SAFE;
